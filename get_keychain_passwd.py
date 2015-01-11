8c1ec87e02e3905d1684af345c1d26e9b3f22acd
Tony Xu <tonylixu@gmail.com>
2015-01-11 16:13:09 -0500

    Add get_generic_passwd function, now you can search generic account's password! Such as HomeWifi. 	renamed:    get_internet_passwd.py -> get_keychain_passwd.py

--- get_keychain_passwd.py
+++ get_keychain_passwd.py
@@ -8,12 +8,14 @@ returns password for given account.
     Example:
         get_keychain_pass('tony@gmail.com', 'imap.gmail.com') retunrs
     tony@gmail.com password.
+        get_generic_pass('HomeWifi') returns HomeWifi password.
 '''
 import re
 import subprocess
 
 def main():
     get_keychain_pass('tony@gmail.com', 'imap.gmail.com')
+    get_generic_pass('NETGEAR42-5G-2')
 
 def get_keychain_pass(account=None, server=None):
     '''Search OSX keychain app and return password if found
@@ -41,6 +43,42 @@ def get_keychain_pass(account=None, server=None):
     command = 'sudo {0} -v {1} -g -a {2} -s {3} {4}'.format(
         params['security'], params['command'], params['account'],
         params['server'], params['keychain'])
+
+    try:
+        output = subprocess.check_output(
+            command, shell=True, stderr=subprocess.STDOUT
+        )
+    except subprocess.CalledProcessError as err:
+        print('FAILED: {0}'.format(err))
+        raise
+
+    for line in output.splitlines():
+        if line.startswith('password: '):
+            outtext = line
+
+    password = re.match(r'password: "(.*)"', outtext).group(1)
+    print('password is {0}'.format(password))
+    return password
+
+def get_generic_pass(account=None):
+    '''Search OSX keychain app and return generic password
+    on account
+
+    Example:
+        get_generic_pass('HomeWifi') will return the password
+    of 'HomeWifi'
+
+    Args:
+        account: account in string
+    '''
+
+    params = {
+            'security': '/usr/bin/security',
+            'command': 'find-generic-password',
+            'account': account,
+        }
+    command = 'sudo {0} -v {1} -g -a {2}'.format(
+        params['security'], params['command'], params['account'])
 
     try:
         output = subprocess.check_output(
