1bbdc2e00039bfdfc1641124246a839107a5640d
tonylixu@gmail.com <tonylixu@gmail.com>
2017-10-02 16:38:07 -0400

    Create sort-dictionary.py file

---
+++ sort-dictionary.py
@@ -0,0 +1,16 @@
+from __future__ import print_function
+'''This script sorts a dictionary of lists by value using the
+third item in the list.
+
+    Input: A dictionary
+    Output: Sorted dictionary
+
+    Exmaple:
+    students = {
+        'John': ['A', 15],
+        'Jane': ['A', 12],
+        'Dave': ['A', 13]    
+    }
+    $ print_function(students)
+    { 'Jane': ['A', 12], 'Dave': ['A', 13], 'John': ['A', 15] }
+'''
