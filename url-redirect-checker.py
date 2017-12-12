12ad4e3bd9743e4fbc569b875088d274645be13e
tonylixu@gmail.com <tonylixu@gmail.com>
2017-12-12 16:13:15 -0500

    Added check_for_redirects function

--- url-redirect-checker.py
+++ url-redirect-checker.py
@@ -1,3 +1,17 @@
 '''
 This python script checks whether a list of given URLs been redirected.
 '''
+import sys
+import requests
+
+def check_for_redirects(url):
+    try:
+        r = requests.get(url, allow_redirects=False, timeout=0.5)
+        if 300 <= r.status_code < 400:
+            return r.headers['location']
+        else:
+            return '[no redirect]'
+    except requests.exceptions.Timeout:
+        return '[timeout]'
+    except requests.exceptions.ConnectionError:
+        return '[connection error]'
