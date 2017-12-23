a60b01dbf25d12999ffc2a793466b3eb191f714e
Tony Li Xu <tonylixu@gmail.com>
2017-12-23 18:02:00 -0500

    Added free memory python file

---
+++ free-mem.py
@@ -0,0 +1,9 @@
+"""
+A Python version of command line "free" command
+
+Usage:
+    $ python free-mem.py
+             total       used       free     shared    buffers      cache
+Mem:      10125520    8625996    1499524          0     349500    3307836
+Swap:            0          0          0
+"""
