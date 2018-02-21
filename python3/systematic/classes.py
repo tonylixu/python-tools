d9aaa5d655a6401700910079eff451c19b2b4286
tonylixu@gmail.com <tonylixu@gmail.com>
2018-02-20 20:52:12 -0500

    Import loger

--- python3/systematic/classes.py
+++ python3/systematic/classes.py
@@ -3,3 +3,10 @@ Common wrapper classes defintion
 """
 import os
 import pwd
+
+# STDOUT: a special value that can be used as stderr argument and
+# make standard error goes to the same handler
+# check_output: Run command and returns its output
+# CalledProcessError: An exception
+from subprocess import STDOUT, check_output, CalledProcessError
+from systematic.log import Logger, LoggerError
