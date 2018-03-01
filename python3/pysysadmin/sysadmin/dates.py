1f3e8c473deab84d86533a64f607a4eaacd8b60a
tonylixu@gmail.com <tonylixu@gmail.com>
2018-02-28 20:45:50 -0500

    Define default parameters

--- python3/pysysadmin/sysadmin/dates.py
+++ python3/pysysadmin/sysadmin/dates.py
@@ -6,3 +6,10 @@ import calendar
 
 from time import localtime, struct_time
 from datetime import datetime, date, timedelta
+
+# Define default date format
+DEFAULT_DATE_FORMAT = '%Y-%m-%d'
+# Define default first day of week
+DEFAULT_WEEK_START_DAY = 1
+# Define default number of workdays per week
+DEFAULT_WORKDAYS_PER_WEEK = 5
