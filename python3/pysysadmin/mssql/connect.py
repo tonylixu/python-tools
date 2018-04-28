6243a00b3e4f73a0d944bb4cd55d66d6a67b03c0
Tony Li Xu <txu@Tonys-MBP.home>
2018-04-27 21:46:13 -0400

    Created connecto_to_db function

--- python3/pysysadmin/mssql/connect.py
+++ python3/pysysadmin/mssql/connect.py
@@ -6,5 +6,19 @@
     Todo:
         * N/A
 """
-import json
-import pypyodbc
+import json        # Provide Json format operations
+import pypyodbc    # The connector lib
+
+def connect_to_db(table='test')
+    """Database connection handler
+
+    Define the database connection string and connect
+    to the database server. After a successful connection, a database query
+    will be executed and the query result will saved to a JSON file
+
+    Args:
+        table (str): The table name
+    
+    Returns:
+        N/A
+    """
